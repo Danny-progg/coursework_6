3d4ed00787dc2f80bb57e7eacb45acbe9a0f8d14
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-10-13 19:42:03 +0300

    Commit 1 - add app in settings.py

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'mailing',
 ]
 
 ROOT_URLCONF = 'config.urls'
